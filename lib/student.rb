0f3e0df59090e405b8e589a813b29c443602515c
Paul Ashour <paul.ashour@gmail.com>
2019-01-20 23:37:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ class Student
   def self.all_students_in_grade_9
     sql = <<-SQL
               SELECT * FROM students
-              WHERE grade = 9;
+              WHERE grade = '9';
             SQL
     DB[:conn].execute(sql).map do |row|
       find_by_name(row)
