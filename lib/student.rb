ac6104edbff470ff5f132dfebb634e0a37de3309
Paul Ashour <paul.ashour@gmail.com>
2019-01-21 01:10:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,7 +81,7 @@ class Student
     first_X_students_in_grade_10(1)[0]
   end
   
-  def self.all_students_in_grade_x(x)
+  def self.all_students_in_grade_X(x)
     sql = <<-SQL
               SELECT * FROM students
               WHERE grade = ?
